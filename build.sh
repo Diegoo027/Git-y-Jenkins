5528f9b181704e39f9fd5d981d402d4226f19a97
diego <diego.moreno5@educa.madrid.org>
2024-11-17 19:15:27 +0100

    ejer3

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 touch index.html
 
+touch pag1.html
+touch pag2.html
+touch pag3.html
+
 cat >index.html<<EOF
 <!DOCTTPE html>
 <html>
@@ -12,6 +16,9 @@ cat >index.html<<EOF
 <body>
 <h1>Página de Diego</h1>
 <p>página creada por Diego</p>
+<li><a href='pag1.html'>Página 1</li>
+<li><a href='pag2.html'>Página 3</li>
+<li><a href='pag3.html'>Página 2</li>
 </body>
 </html>
 EOF
