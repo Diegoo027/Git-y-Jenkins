9773f8be186c6d81e48d09cade6ab5b7ca0eb0d8
diego <diego.moreno5@educa.madrid.org>
2024-11-17 18:46:39 +0100

    ejer2

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/user/bin/bash
+
+touch index.html
+
+cat > index html <<EOF
+<!DOCTTPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>pagina trabajo</title>
+</head>
+<body>
+<h1>Página de Diego</h1>
+<p>página creada por Diego</p>
+</body>
+</html>
+EOF
